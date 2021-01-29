c0a60f0bc9d2a999fea88fd5de59fc77405f7d57
navitacion <masataka.i.navi@gmail.com>
2021-01-29 20:33:39 +0900

    bug fix

--- models/model.py
+++ models/model.py
@@ -26,16 +26,17 @@ class LGBMModel(BaseModel):
     def __init__(self, params):
         super(LGBMModel, self).__init__(params)
 
-    def train(self, X_train, y_train, X_val, y_val):
+    def train(self, X_train, y_train, X_val, y_val, feature_name='auto'):
         train_data = lgb.Dataset(X_train, label=y_train)
         valid_data = lgb.Dataset(X_val, label=y_val, reference=train_data)
 
         self.model = lgb.train(self.params,
-                          train_data,
-                          valid_sets=[valid_data, train_data],
-                          valid_names=['eval', 'train'],
-                          verbose_eval=1000,
-                          )
+                               train_data,
+                               valid_sets=[valid_data, train_data],
+                               valid_names=['eval', 'train'],
+                               feature_name=feature_name,
+                               verbose_eval=5000,
+                               )
 
         oof = self.model.predict(X_val, num_iteration=self.model.best_iteration)
 
@@ -60,8 +61,8 @@ class CatBoostModel(BaseModel):
         valid_data = Pool(X_val, label=y_val)
         self.model = CatBoostRegressor(**self.params)
         self.model.fit(train_data,
-                  eval_set=valid_data,
-                  use_best_model=True)
+                       eval_set=valid_data,
+                       use_best_model=True)
 
         oof = self.model.predict(X_val)
 
--- models/trainer.py
+++ models/trainer.py
@@ -3,10 +3,11 @@ import numpy as np
 import pandas as pd
 
 class Trainer:
-    def __init__(self, model, id_col, tar_col, features, cv, criterion, experiment):
+    def __init__(self, model, id_col, tar_col, g_col, features, cv, criterion, experiment):
         self.model = model
         self.id_col = id_col
         self.tar_col = tar_col
+        self.g_col = g_col
         self.features = features
         self.cv = cv
         self.criterion = criterion
@@ -33,11 +34,12 @@ class Trainer:
         # Split Train, Test Dataset
         train = df[df['is_train'] == 1].reset_index(drop=True)
         test = df[df['is_train'] == 0].reset_index(drop=True)
+        drop_cols = ['is_train', self.g_col, self.id_col, self.tar_col]
 
         if self.features is None:
-            self.features = [c for c in df.columns if c not in ['is_train', self.id_col, self.tar_col]]
+            self.features = [c for c in df.columns if c not in drop_cols]
         else:
-            self.features = [c for c in self.features if c not in ['is_train', self.id_col, self.tar_col]]
+            self.features = [c for c in self.features if c not in drop_cols]
 
         if mode == 'fit':
             # Train
@@ -45,12 +47,17 @@ class Trainer:
             self.y_train = train[self.tar_col].values
             self.y_train = self._transform_value(self.y_train, mode='forward')
             self.train_id = train[self.id_col].values
-            self.publisher = train['Publisher']
+            self.group = train[self.g_col]
 
         if mode == 'predict':
             # Test
             self.X_test = test[self.features].values
             self.test_id = test[self.id_col].values
+
+        # init Model list
+        self.models = []
+        self.oof_pred = np.zeros(len(self.y_train))
+        self.oof_y = np.zeros(len(self.y_train))
 
 
     def _transform_value(self, v, mode='forward'):
@@ -71,33 +78,58 @@ class Trainer:
             Output value
         """
         if mode == 'forward':
-            out = np.log1p(v)
+            # out = np.log1p(v)
+            out = v
         elif mode == 'backward':
-            v = np.where(v < 0, 0, v)
-            out = np.expm1(v)
+            # v = np.where(v < 0, 0, v)
+            # out = np.expm1(v)
+            out = v
         else:
             out = v
 
         return out
 
 
-    def _train_cv(self):
+    def _train_cv_group(self):
         """
         Train loop for Cross Validation
         """
-        self.models = []
-        self.oof_pred = np.zeros(len(self.y_train))
-        self.oof_y = np.zeros(len(self.y_train))
+        # GroupKFold
+        # ref: https://www.guruguru.science/competitions/13/discussions/cc7167cb-3627-448a-b9eb-7afcd29fd122/
+        unique_year = self.group.unique()
+        for i, (tr_group_idx, va_group_idx) in enumerate(self.cv.split(unique_year)):
+            tr_groups, va_groups = unique_year[tr_group_idx], unique_year[va_group_idx]
 
-        for i, (_trn_idx, _val_idx) in enumerate(self.cv.split(self.publisher.unique())):
-            tr_groups, val_groups = self.publisher.unique()[_trn_idx], self.publisher.unique()[_val_idx]
-            trn_idx = self.publisher.isin(tr_groups)
-            val_idx = self.publisher.isin(val_groups)
+            trn_idx = self.group.isin(tr_groups)
+            val_idx = self.group.isin(va_groups)
 
             X_trn, y_trn = self.X_train[trn_idx], self.y_train[trn_idx]
             X_val, y_val = self.X_train[val_idx], self.y_train[val_idx]
 
-            oof = self.model.train(X_trn, y_trn, X_val, y_val)
+            oof = self.model.train(X_trn, y_trn, X_val, y_val, feature_name=self.features)
+
+            # Score
+            oof = self._transform_value(oof, mode='backward')
+            y_val = self._transform_value(y_val, mode='backward')
+            score = self.criterion(y_val, oof)
+
+            # Logging
+            self.experiment.log_metric('Fold_score', score, step=i + 1)
+            print(f'Fold {i + 1}  Score: {score:.3f}')
+            self.oof_pred[val_idx] = oof
+            self.oof_y[val_idx] = y_val
+            self.models.append(self.model)
+
+
+    def _train_cv(self):
+        """
+        Train loop for Cross Validation
+        """
+        for i, (trn_idx, val_idx) in enumerate(self.cv.split(self.X_train, self.y_train)):
+            X_trn, y_trn = self.X_train[trn_idx], self.y_train[trn_idx]
+            X_val, y_val = self.X_train[val_idx], self.y_train[val_idx]
+
+            oof = self.model.train(X_trn, y_trn, X_val, y_val, feature_name=self.features)
 
             # Score
             oof = self._transform_value(oof, mode='backward')
@@ -127,13 +159,11 @@ class Trainer:
             self.tar_col: self._transform_value(self.oof_pred, mode='backward')
         })
 
-        oof = oof.sort_values(by='id')
-
-        oof.loc[oof[self.tar_col] < 0, self.tar_col] = 0
+        oof = oof.sort_values(by=self.id_col)
 
         # Logging
         sub_name = f'oof_score_{self.oof_score:.4f}.csv'
-        oof[[self.tar_col]].to_csv(os.path.join(sub_name), index=False)
+        oof[[self.id_col, self.tar_col]].to_csv(os.path.join(sub_name), index=False)
         self.experiment.log_asset(file_data=sub_name, file_name=sub_name)
         os.remove(sub_name)
 
@@ -163,19 +193,20 @@ class Trainer:
             self.tar_col: self.preds
         })
 
-        sub = sub.sort_values(by='id')
-
-        sub.loc[sub[self.tar_col] < 0, self.tar_col] = 0
+        sub = sub.sort_values(by=self.id_col)
 
         # Logging
         sub_name = f'sub_score_{self.oof_score:.4f}.csv'
-        sub[[self.tar_col]].to_csv(os.path.join(sub_name), index=False)
+        sub[[self.id_col, self.tar_col]].to_csv(os.path.join(sub_name), index=False)
         self.experiment.log_asset(file_data=sub_name, file_name=sub_name)
         os.remove(sub_name)
 
     def fit(self, df):
         self._prepare_data(df, mode='fit')
-        self._train_cv()
+        if self.g_col is None:
+            self._train_cv()
+        else:
+            self._train_cv_group()
         self._train_end()
 
 
@@ -187,17 +218,21 @@ class Trainer:
 
     def get_feature_importance(self):
         assert len(self.models) != 0, "You Must Train Model!!"
-        feat_imp = np.zeros(len(self.features))
+        feat_imp_df = pd.DataFrame(self.features, columns=['feature'])
+        tar_col =[]
 
-        for m in self.models:
-            feat_imp += m.get_feature_importance()
+        for i, m in enumerate(self.models):
+            # make DataFrame from Model Instance
+            tmp_feat = pd.DataFrame({'feature': m.model.feature_name(), f'importance_{i}': m.get_feature_importance()})
+            feat_imp_df = feat_imp_df.merge(tmp_feat, on='feature', how='left')
+            # add Importance Name
+            tar_col.append(f'importance_{i}')
+        feat_imp_df.fillna(0, inplace=True)
 
-        feat_imp = feat_imp / len(self.models)
+        feat_imp_df['importance'] = feat_imp_df[tar_col].sum(axis=1)
+        feat_imp_df['importance'] /= len(self.models)
+        feat_imp_df = feat_imp_df[['feature', 'importance']]
 
-        feat_imp_df = pd.DataFrame({
-            'feature': self.features,
-            'importance': feat_imp
-        })
         feat_imp_df.sort_values(by='importance', ascending=False, inplace=True)
         feat_imp_df.reset_index(drop=True, inplace=True)
         feat_imp_df.to_csv('feature_importance.csv', index=False)
